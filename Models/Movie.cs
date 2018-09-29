9a000414de5f6f4efac0eb12084105fc367e526c
laurGeo <lauren.geoghegan7@mail.dcu.ie>
2018-09-29 23:04:58 +0100

    First commit

---
+++ Models/Movie.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        //ID required for db
+        public int ID {get;set;} 
+        public string Title {get;set;}
+        public DateTime ReleaseDate {get;set;}
+        public string Genre {get;set;}
+        public decimal Price {get;set;}
+    }
+}
